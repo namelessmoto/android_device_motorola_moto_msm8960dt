6a66650f076f1a0e375b3873fef7a67ed55aa2b0
Validus13 <groundzeroroms@gmail.com>
2014-04-06 13:51:08 -0400

    Update and rename du.mk to omni_moto_msm8960dt.mk

--- omni_moto_msm8960dt.mk
+++ omni_moto_msm8960dt.mk
@@ -1,9 +1,9 @@
 # Inherit some common LS stuff.
-$(call inherit-product, vendor/du/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
-$(call inherit-product, vendor/du/config/telephony.mk)
+$(call inherit-product, vendor/omni/config/telephony.mk)
 
-$(call inherit-product, vendor/du/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/cdma.mk)
 
 # Enhanced NFC
 #$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
@@ -13,6 +13,6 @@ TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
-PRODUCT_NAME := du_moto_msm8960dt
+PRODUCT_NAME := omni_moto_msm8960dt
 
 $(call inherit-product, device/motorola/moto_msm8960dt/full_moto_msm8960dt.mk)
